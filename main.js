2fa6303b97331f3eef44dd26ab6b75ed50356a7f
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-09-19 14:38:53 -0700

    v-for

--- main.js
+++ main.js
@@ -10,6 +10,20 @@ var app = new Vue({
     inStock: true,
     inventory: 100,
     onSale: false,
-    details: ["80% cotton",  "20% polyester", "Gender-neutral"]
+    details: [
+        "80% cotton",
+        "20% polyester",
+        "Gender-neutral"
+    ],
+    variants: [
+        {
+            variantId: 2234,
+            variantColor: 'green'
+        },
+        {
+            variantId: 2235,
+            variantColor: 'blue'
+        }
+    ]
   }
 });
