031914491f5d67fa6f77870b016a08766b682f55
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-09-19 13:44:27 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//This data can change and this is great!
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'A pair of warm, fuzzy socks',
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    altText: 'A pair of socks',
+    link: 'https://www.uttpic.com'
+  }
+})
